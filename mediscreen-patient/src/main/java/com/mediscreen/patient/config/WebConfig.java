79dd1fd4f8407a35874266689d5cc5e4528203cb
chanwooKIM <chanwookim@MacBook-Pro-de-chanwooKIM.local>
2022-06-01 08:12:40 +0200

    Feat: Add CORS settings

--- mediscreen-patient/src/main/java/com/mediscreen/patient/config/WebConfig.java
+++ mediscreen-patient/src/main/java/com/mediscreen/patient/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.mediscreen.patient.config;
 
 import com.mediscreen.patient.common.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,4 +15,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**");
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("http://localhost");
+    }
+
+
 }
